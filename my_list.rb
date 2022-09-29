83e57447776d0aab7cf3f69395d00dc75708df3d
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-09-29 14:56:01 -0300

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -2,8 +2,20 @@ require_relative 'my_enumerable.rb'
 
 class MyList
   include MyEnumerable
+  @list = []
+
+  def initialize(*elements)
+    @list = elements
+  end
 end
 
-list = MyList.new
+list = MyList.new(1, 2, 3, 4) #<MyList: @list=[1, 2, 3, 4]>
 
-list.all
+puts list.all? {|e| e < 5} # true
+puts list.all? {|e| e > 5} # false
+
+puts list.any? {|e| e == 2} #true
+puts list.any? {|e| e == 5} # false
+
+puts print list.filter {|e| e.even?} # [2, 4]
+print list.filter {|e| e.odd?} # [1, 3]
